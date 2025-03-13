a2458d65c7777f642c1868b4f45a19437be2ee6c
ModalesXD <noebradlu@gmail.com>
2025-03-12 21:24:47 -0600

    Aded Stable structure

--- src/index.ts
+++ src/index.ts
@@ -1,71 +1,15 @@
-import {Client, Events, GatewayIntentBits, Collection, CommandInteraction, MessageFlags} from 'discord.js';
-import * as path from 'path';
-import * as fs from 'fs';
+import { Client, GatewayIntentBits } from 'discord.js';
+import { loadCommands } from './handlers/commandHandler';
+import { loadEvents } from './events/interactionCreate';
 
-interface ExtendedClient extends Client {
-    commands: Collection<unknown, unknown>;
-}
+const Intents = GatewayIntentBits
+const client = new Client({ intents: [Intents.GuildMessages, Intents.Guilds, Intents.GuildIntegrations, Intents.MessageContent] });
 
-
-const client: ExtendedClient = Object.assign(new Client({ 
-    intents: [
-        GatewayIntentBits.Guilds,
-        GatewayIntentBits.GuildMessages,
-        GatewayIntentBits.MessageContent,
-        GatewayIntentBits.GuildMembers,
-        GatewayIntentBits.GuildPresences,
-        GatewayIntentBits.GuildMessageReactions
-    ]
-}), { commands: new Collection() });
-
-client.commands = new Collection();
-
-const foldersPath = path.join(__dirname, 'commands');
-const commandFolders = fs.readdirSync(foldersPath);
-
-for (const folder of commandFolders) {
-	const commandsPath = path.join(foldersPath, folder);
-	const commandFiles = fs.readdirSync(commandsPath).filter(file => file.endsWith('.js'));
-	for (const file of commandFiles) {
-		const filePath = path.join(commandsPath, file);
-		const command = require(filePath);
-		// Set a new item in the Collection with the key as the command name and the value as the exported module
-		if ('data' in command && 'execute' in command) {
-			client.commands.set(command.data.name, command);
-		} else {
-			console.log(`[WARNING] The command at ${filePath} is missing a required "data" or "execute" property.`);
-		}
-	}
-}
-
-client.once(Events.ClientReady, c => {
-	console.log(`Listening Logged with ${c.user.tag}`);
+client.once('ready', () => {
+    console.log('Bot is ready!');
 });
+ 
+loadEvents(client);   // Cargar eventos
+loadCommands(client); // Cargar comandos
 
-client.on(Events.InteractionCreate, async interaction => {
-	if (!interaction.isChatInputCommand()) return;
-
-	const command = interaction.client.commands.get(interaction.commandName);
-
-	if (!command) {
-		console.error(`No command matching ${interaction.commandName} was found.`);
-		return;
-	}
-
-	try {
-		await command.execute(interaction);
-	} catch (error) {
-		console.error(error);
-		if (interaction.replied || interaction.deferred) {
-			await interaction.followUp({ content: 'There was an error while executing this command!', flags: MessageFlags.Ephemeral });
-		} else {
-			await interaction.reply({ content: 'There was an error while executing this command!', flags: MessageFlags.Ephemeral });
-		}
-	}
-});
-
-
-client.commands = new Collection();
-
-client.login(process.env.DISCORD_TOKEN)
-
+client.login(process.env.BOT_TOKEN);
